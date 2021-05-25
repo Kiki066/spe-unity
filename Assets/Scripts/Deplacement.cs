478b0ad75d0df4863b0f927dcfe4d71e0c64b575
Kiki066 <Violet.k46@gmail.com>
2021-05-25 10:42:04 +0200

    deplacement

---
+++ Assets/Scripts/Deplacement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deplacement : MonoBehaviour
+{
+
+
+    [SerializeField]        //permet de faire apparaitre speed dans Unity
+    private float speed = 3f;
+
+    [SerializeField]
+    private float mouseSensitivityX = 3f;
+
+    [SerializeField]
+    private float mouseSensitivityY = 3f;
+
+    private PlayerMotor motor;
+
+    private void Start()
+    {
+        motor = GetComponent<PlayerMotor>();    //Stocke le script PlayerMotor dans Motor
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Calculer la vitesse du mouvement de notre joueur
+        float xMov = Input.GetAxis("Horizontal");  // Input.GetAxisRaw retourne les valeurs 1 ou -1
+        float zMov = Input.GetAxis("Vertical");
+
+        Vector3 moveHorizontal = transform.right * xMov;    //Prend le champs right du composant "transform" de Unity.
+        Vector3 moveVertical = transform.forward * zMov;
+
+        Vector3 velocity = (moveHorizontal + moveVertical) * speed;
+
+        motor.Move(velocity);
+    }
+}
---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    private Vector3 velocity;
+    private Vector3 rotation;
+    private float cameraRotationX = 0f;  //Bloquer la rotation sur l'axe X
+    private float currentCameraRotationX = 0f; //valeur actuelle
+    private Rigidbody rb;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    public void Rotate(Vector3 _rotation)
+    {
+        rotation = _rotation;
+    }
+}
